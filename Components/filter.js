8864868f8c1dfce92311bc7f3274efbcfb65bf7e
Thanate <nuk.thanate.d@gmail.com>
2024-12-02 10:05:09 +0700

    feat:add filter drop down

--- Components/filter.js
+++ Components/filter.js
@@ -1,10 +1,14 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Filter() {
   const [movies, setMovies] = useState([]);
   const [languages, setLanguages] = useState([]);
   const [genres, setGenres] = useState([]);
   const [cities, setCities] = useState([]);
+  const [releaseDate, setReleaseDate] = useState(null);
   const [selectedFilters, setSelectedFilters] = useState({
     movie: "",
     language: "",
@@ -12,13 +16,12 @@ export default function Filter() {
     city: "",
   });
 
-  // ดึงข้อมูลจาก API
+  // Fetch Filters Data
   useEffect(() => {
     const fetchFiltersData = async () => {
       try {
-        const response = await fetch("/api/filterMovie");
-        const data = await response.json();
-        console.log("API Data:", data); // Debug ข้อมูลจาก API
+        const response = await axios.get("/api/filterMovie");
+        const data = response.data;
         setMovies(data.movies || []);
         setLanguages(data.languages || []);
         setGenres(data.genres || []);
@@ -31,18 +34,6 @@ export default function Filter() {
     fetchFiltersData();
   }, []);
 
-  // ฟังก์ชันกรองค่าซ้ำ
-  const getUniqueItems = (items, key) => {
-    return Array.from(new Set(items.map((item) => item[key]))).map(
-      (uniqueKey) => items.find((item) => item[key] === uniqueKey)
-    );
-  };
-
-  // สร้างรายการหนังที่ไม่ซ้ำ
-  const uniqueMovies = getUniqueItems(movies, "movie_name");
-  console.log("Unique Movies:", uniqueMovies); // Debug รายการหนัง
-
-  // ฟังก์ชัน handle การเปลี่ยนค่าของ dropdown
   const handleFilterChange = (key, value) => {
     setSelectedFilters((prev) => ({
       ...prev,
@@ -50,10 +41,24 @@ export default function Filter() {
     }));
   };
 
-  // ฟังก์ชัน handle การค้นหา
-  const handleSearch = () => {
-    console.log("Filters applied:", selectedFilters);
-    // ทำการ fetch หรือกรองข้อมูลตามค่าที่เลือก
+  const handleSearch = async () => {
+    const formattedDate = releaseDate
+      ? releaseDate.toISOString().split("T")[0]
+      : null;
+
+    const filters = {
+      ...selectedFilters,
+      releaseDate: formattedDate,
+    };
+
+    console.log("Filters applied:", filters);
+
+    try {
+      const response = await axios.post("/api/filterMovie", filters);
+      console.log("Filtered Movies:", response.data);
+    } catch (error) {
+      console.error("Failed to fetch filtered movies:", error);
+    }
   };
 
   return (
@@ -68,9 +73,9 @@ export default function Filter() {
               onChange={(e) => handleFilterChange("movie", e.target.value)}
             >
               <option value="">Movie</option>
-              {uniqueMovies.map((movie) => (
+              {movies.map((movie) => (
                 <option key={movie.movie_id} value={movie.movie_id}>
-                  {movie.movie_name}
+                  {movie.title}
                 </option>
               ))}
             </select>
@@ -84,16 +89,11 @@ export default function Filter() {
               onChange={(e) => handleFilterChange("language", e.target.value)}
             >
               <option value="">Language</option>
-              {languages && languages.length > 0
-                ? languages.map((language) => (
-                    <option
-                      key={language.language_id}
-                      value={language.language_id}
-                    >
-                      {language.name}
-                    </option>
-                  ))
-                : null}
+              {languages.map((language) => (
+                <option key={language.language_id} value={language.language_id}>
+                  {language.name}
+                </option>
+              ))}
             </select>
 
             <select
@@ -102,17 +102,15 @@ export default function Filter() {
               onChange={(e) => handleFilterChange("genre", e.target.value)}
             >
               <option value="">Genre</option>
-              {genres && genres.length > 0
-                ? genres.map((genre) => (
-                    <option key={genre.genre_id} value={genre.genre_id}>
-                      {genre.name}
-                    </option>
-                  ))
-                : null}
+              {genres.map((genre) => (
+                <option key={genre.genre_id} value={genre.genre_id}>
+                  {genre.name}
+                </option>
+              ))}
             </select>
           </div>
 
-          {/* City */}
+          {/* City and Release Date */}
           <div className="flex gap-4 mb-4 lg:mb-0 lg:flex-row lg:flex-1 lg:gap-4">
             <div className="flex-1">
               <select
@@ -121,14 +119,41 @@ export default function Filter() {
                 onChange={(e) => handleFilterChange("city", e.target.value)}
               >
                 <option value="">City</option>
-                {cities && cities.length > 0
-                  ? cities.map((city) => (
-                      <option key={city.city_id} value={city.city_id}>
-                        {city.name}
-                      </option>
-                    ))
-                  : null}
+                {cities.map((city) => (
+                  <option key={city.city_id} value={city.city_id}>
+                    {city.name}
+                  </option>
+                ))}
               </select>
+            </div>
+
+            {/* <div className="flex-1 relative">
+              <ReactDatePicker
+                selected={releaseDate}
+                onChange={(date) => setReleaseDate(date)}
+                className="w-full p-2 border border-[#565f7e] py-4 bg-[#21263f] text-[#8b93b0] text-xl rounded focus:outline-none"
+                placeholderText="Release Date"
+              />
+              <span className="absolute right-12 top-1/2 -translate-y-1/2 text-gray-400">
+                <img
+                  src="/img/Date_today_light.png"
+                  alt="Calendar Icon"
+                  className="h-6 w-6"
+                />
+              </span>
+            </div> */}
+
+            <div className="flex-1 relative">
+              <div className="w-full p-2 border border-[#565f7e] py-4 bg-[#21263f] text-[#8b93b0] text-xl rounded cursor-pointer focus:outline-none flex items-center">
+                <span className="text-gray-400">Release date</span>
+                <span className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
+                  <img
+                    src="/img/Date_today_light.png"
+                    alt="Calendar Icon"
+                    className="h-5 w-5"
+                  />
+                </span>
+              </div>
             </div>
           </div>
 
--- pages/api/filterMovie.js
+++ pages/api/filterMovie.js
@@ -6,23 +6,52 @@ export default async function handler(req, res) {
       const result = await connectionPool.query(`
         SELECT 
         m.movie_id,
-    m.title,
+        m.title,
         l.name AS language,
-    STRING_AGG(g.name, ', ') AS genres
-FROM 
-    movies m
-INNER JOIN 
-    languages l ON m.language_id = l.language_id
-LEFT JOIN 
-    movie_genres mg ON m.movie_id = mg.movie_id
-LEFT JOIN 
-    genres g ON mg.genre_id = g.genre_id
-GROUP BY 
+        STRING_AGG(g.name, ', ') AS genres
+        FROM 
+        movies m
+        INNER JOIN 
+        languages l ON m.language_id = l.language_id
+        LEFT JOIN 
+        movie_genres mg ON m.movie_id = mg.movie_id
+        LEFT JOIN 
+        genres g ON mg.genre_id = g.genre_id
+        GROUP BY 
         m.movie_id, m.title, l.name
-ORDER BY 
-    m.title            
+        ORDER BY 
+        m.title            
             `);
-      res.status(200).json({ movies: result.rows });
+            
+      const languagesResult = await connectionPool.query(`
+        SELECT language_id, name
+        FROM languages
+        ORDER BY name
+      `);
+
+      const genresResult = await connectionPool.query(`
+        SELECT DISTINCT g.genre_id, g.name
+        FROM genres g
+        ORDER BY g.name
+      `);
+
+      const citiesResult = await connectionPool.query(`
+        SELECT DISTINCT
+          c.city_id,
+          c.city_name AS name
+        FROM
+          cities c
+        INNER JOIN cinemas ci ON c.city_id = ci.city_id
+        ORDER BY
+          c.city_name;
+      `);
+
+      res.status(200).json({
+        movies: result.rows,
+        languages: languagesResult.rows,
+        genres: genresResult.rows,
+        cities: citiesResult.rows,
+      });
     } catch (error) {
       console.error("Error fetching movies and reviews:", error.message);
       res.status(500).json({ error: "Failed to fetch data" });
