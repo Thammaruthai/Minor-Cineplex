338bb639a21fa4113f73e740253246a4d5c55ce3
Thammaruthai <thammaruthai@gmail.com>
2024-12-04 10:40:42 +0700

    feat:add hall condition

--- pages/addhall/index.js
+++ pages/addhall/index.js
@@ -66,6 +66,7 @@ export default function AddHall() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    console.log(name, value);
 
     if (name === "city") {
       if (value === "create_new") {
