ecf1e2ffb1fe771704454463c31f27323b4c5c7c
Thammaruthai <thammaruthai@gmail.com>
2024-12-04 13:31:26 +0700

    feat : add animation on register page

--- pages/api/addhall/add-hall.js
+++ pages/api/addhall/add-hall.js
@@ -15,83 +15,83 @@ export default async function handler(req, res) {
 
   const client = await connectionPool.connect();
 
-  // try {
-  //   await client.query("BEGIN"); // Start transaction
+  try {
+    await client.query("BEGIN"); // Start transaction
 
-  //   // Check or insert city
-  //   let cityId;
-  //   const cityCheckResult = await client.query(
-  //     `SELECT city_id FROM cities WHERE city_name = $1`,
-  //     [city]
-  //   );
+    // Check or insert city
+    let cityId;
+    const cityCheckResult = await client.query(
+      `SELECT city_id FROM cities WHERE city_name = $1`,
+      [city]
+    );
 
-  //   console.log(cityCheckResult);
+    console.log(cityCheckResult);
 
-  //   if (cityCheckResult.rows.length > 0) {
-  //     cityId = cityCheckResult.rows[0].city_id;
-  //   } else {
-  //     const cityInsertResult = await client.query(
-  //       `INSERT INTO cities (city_name, created_by, updated_by, updated_at)
-  //        VALUES ($1, $2, $2, NOW())
-  //        RETURNING city_id`,
-  //       [city, createdBy]
-  //     );
-  //     cityId = cityInsertResult.rows[0].city_id;
-  //   }
+    if (cityCheckResult.rows.length > 0) {
+      cityId = cityCheckResult.rows[0].city_id;
+    } else {
+      const cityInsertResult = await client.query(
+        `INSERT INTO cities (city_name, created_by, updated_by, updated_at)
+         VALUES ($1, $2, $2, NOW())
+         RETURNING city_id`,
+        [city, createdBy]
+      );
+      cityId = cityInsertResult.rows[0].city_id;
+    }
 
-  //   // Check or insert cinema
-  //   let cinemaId;
-  //   const cinemaCheckResult = await client.query(
-  //     `SELECT cinema_id FROM cinemas WHERE name = $1 AND city_id = $2`,
-  //     [cinema, cityId]
-  //   );
+    // Check or insert cinema
+    let cinemaId;
+    const cinemaCheckResult = await client.query(
+      `SELECT cinema_id FROM cinemas WHERE name = $1 AND city_id = $2`,
+      [cinema, cityId]
+    );
 
-  //   console.log(cinemaCheckResult);
+    console.log(cinemaCheckResult);
 
-  //   if (cinemaCheckResult.rows.length > 0) {
-  //     cinemaId = cinemaCheckResult.rows[0].cinema_id;
-  //   } else {
-  //     const cinemaInsertResult = await client.query(
-  //       `INSERT INTO cinemas (name, city_id, created_by, updated_by, updated_at)
-  //        VALUES ($1, $2, $3, $3, NOW())
-  //        RETURNING cinema_id`,
-  //       [cinema, cityId, createdBy]
-  //     );
-  //     cinemaId = cinemaInsertResult.rows[0].cinema_id;
-  //   }
+    if (cinemaCheckResult.rows.length > 0) {
+      cinemaId = cinemaCheckResult.rows[0].cinema_id;
+    } else {
+      const cinemaInsertResult = await client.query(
+        `INSERT INTO cinemas (name, city_id, created_by, updated_by, updated_at)
+         VALUES ($1, $2, $3, $3, NOW())
+         RETURNING cinema_id`,
+        [cinema, cityId, createdBy]
+      );
+      cinemaId = cinemaInsertResult.rows[0].cinema_id;
+    }
 
-  //   // Check if hall already exists
-  //   const hallCheckResult = await client.query(
-  //     `SELECT hall_id FROM halls WHERE name = $1 AND cinema_id = $2`,
-  //     [hall, cinemaId]
-  //   );
+    // Check if hall already exists
+    const hallCheckResult = await client.query(
+      `SELECT hall_id FROM halls WHERE name = $1 AND cinema_id = $2`,
+      [hall, cinemaId]
+    );
 
-  //   console.log(hallCheckResult);
+    console.log(hallCheckResult);
 
-  //   if (hallCheckResult.rows.length > 0) {
-  //     throw new Error("Hall with the same name already exists.");
-  //   }
+    if (hallCheckResult.rows.length > 0) {
+      throw new Error("Hall with the same name already exists.");
+    }
 
-  //   //Insert hall
-  //   const hallInsertResult = await client.query(
-  //     `INSERT INTO halls (name, cinema_id, seat_capacity, created_by, updated_by, updated_at)
-  //      VALUES ($1, $2, $3, $4, $4, NOW())
-  //      RETURNING hall_id`,
-  //     [hall, cinemaId, seatCapacity, createdBy]
-  //   );
+    //Insert hall
+    const hallInsertResult = await client.query(
+      `INSERT INTO halls (name, cinema_id, seat_capacity, created_by, updated_by, updated_at)
+       VALUES ($1, $2, $3, $4, $4, NOW())
+       RETURNING hall_id`,
+      [hall, cinemaId, seatCapacity, createdBy]
+    );
 
-  //   if (hallInsertResult.rowCount === 0) {
-  //     throw new Error("Failed to add hall");
-  //   }
+    if (hallInsertResult.rowCount === 0) {
+      throw new Error("Failed to add hall");
+    }
 
-  //   await client.query("COMMIT");
-  //   res.status(201).json({ message: "Hall added successfully!" });
-  // } catch (error) {
-  //   await client.query("ROLLBACK");
-  //   console.error("Error adding hall:", error);
-  //   res.status(500).json({ error: error.message || "Internal Server Error" });
-  // } finally {
-  //   client.release();
-  // }
-  res.status(201).json({ message: "Hall added successfully!" });
+    await client.query("COMMIT");
+    res.status(201).json({ message: "Hall added successfully!" });
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.error("Error adding hall:", error);
+    res.status(500).json({ error: error.message || "Internal Server Error" });
+  } finally {
+    client.release();
+  }
+  
 }
--- pages/register/index.js
+++ pages/register/index.js
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
 import Navbar from "@/Components/navbar";
 
-const inputStyle = "border border-gray-200 rounded bg-[#21263F] py-3 pl-4";
-const labelStyle = "text-gray-400";
-const buttonStyleEnabled = "bg-[#4E7BEE] w-full py-3"; // Enabled button style
-const buttonStyleDisabled = "bg-gray-500 w-full py-3 cursor-not-allowed"; // Disabled button style
+const inputStyle =
+  "border border-gray-200 rounded bg-[#21263F] py-3 pl-4 placeholder-[#8B93B0]";
+  const inputErrorStyle =
+    "border border-[#E5364B] rounded bg-[#21263F] py-3 pl-4 placeholder-[#8B93B0]";
+const labelStyle = "text-[#C8CEDD]";
+const buttonStyleEnabled =
+  "bg-[#4E7BEE] w-full py-3 hover:bg-[#1E29A8] active:[#0C1580]"; // Enabled button style
+const buttonStyleDisabled =
+  "bg-gray-500 w-full py-3 cursor-not-allowed"; // Disabled button style
 const inputFieldStyle = "flex flex-col gap-1";
 
 export default function Register() {
@@ -22,6 +27,20 @@ export default function Register() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [loadingText, setLoadingText] = useState("Registering, please wait.");
+
+  // Animate the loading text
+  useEffect(() => {
+    if (isRegistering) {
+      let dots = "";
+      const interval = setInterval(() => {
+        dots = dots.length < 3 ? dots + "." : "";
+        setLoadingText(`Registering, please wait${dots}`);
+      }, 500); // Update every 500ms
+      return () => clearInterval(interval); // Cleanup interval
+    }
+  }, [isRegistering]);
 
   // Function to calculate password strength
   const calculatePasswordStrength = (password) => {
@@ -71,7 +90,7 @@ export default function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setIsRegistering(true);
     try {
       const response = await axios.post("/api/register", formData);
       setMessage(response.data.message);
@@ -80,15 +99,29 @@ export default function Register() {
     } catch (err) {
       setError(err.response?.data?.error || "Something went wrong.");
       setMessage("");
+    } finally {
+      setIsRegistering(false); // End loading state
     }
   };
 
+  if (isRegistering) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-[#070C1B]">
+        <div className="flex flex-col items-center justify-center text-white gap-4">
+          {/* Spinner Animation */}
+          <div className="w-16 h-16 border-4 border-blue-500 border-solid rounded-full border-t-transparent animate-spin"></div>
+          <p className="animate-pulse">{loadingText}</p>
+        </div>
+      </div>
+    );
+  }
+
   if (success) {
     return (
-      <div className="flex flex-col items-center justify-center gap-6 text-white h-screen bg-slate-900 min-h-[640px] min-w-[300px]">
-        <div className="flex flex-col gap-6 w-[380px] rounded-lg text-center max-sm:w-11/12">
+      <div className="flex flex-col items-center justify-center gap-6 text-white h-screen bg-slate-900 min-h-[640px] min-w-[300px] animate-fade-in">
+        <div className="flex flex-col gap-6 w-[380px] rounded-lg text-center max-sm:w-11/12 animate-scale-up">
           <div className="flex flex-col items-center justify-center ">
-            <div className="flex flex-col items-center justify-center w-[80px] h-[80px] rounded-full text-5xl text-white bg-[#00A372]">
+            <div className="flex flex-col items-center justify-center w-[80px] h-[80px] rounded-full text-5xl text-white bg-[#00A372] animate-bounce">
               ✔
             </div>
           </div>
@@ -99,7 +132,7 @@ export default function Register() {
             </p>
           </div>
           <button
-            className="bg-[#4E7BEE] w-full py-3 mt-4"
+            className="bg-[#4E7BEE] w-full py-3 mt-4 hover:bg-[#1E29A8]"
             onClick={() => (window.location.href = "/")}
           >
             {"Back to home"}
@@ -113,7 +146,9 @@ export default function Register() {
     <>
       <div className="w-full flex flex-col h-full items-center justify-center bg-[#070C1B] min-h-[500px] min-w-[300px] max-sm:justify-start max-sm:pt-8">
         <div className="container w-[380px] text-white rounded-lg max-sm:w-11/12">
-          <h1 className="mb-6 text-4xl text-center font-bold">Register</h1>
+          <h1 className="mb-10 text-4xl text-center font-bold h-11 ">
+            Register
+          </h1>
           <form onSubmit={handleSubmit}>
             <div className="flex flex-col gap-y-6">
               <div className={inputFieldStyle}>
@@ -126,6 +161,7 @@ export default function Register() {
                   name="name"
                   value={formData.name}
                   onChange={handleChange}
+                  placeholder="Full name"
                   required
                 />
               </div>
@@ -134,7 +170,8 @@ export default function Register() {
                   Email
                 </label>
                 <input
-                  className={inputStyle}
+                  placeholder="Email"
+                  className={emailError || error ? inputErrorStyle : inputStyle}
                   type="email"
                   name="email"
                   value={formData.email}
@@ -150,7 +187,8 @@ export default function Register() {
                   Password
                 </label>
                 <input
-                  className={inputStyle}
+                  placeholder="Password"
+                  className={emailError || error ? inputErrorStyle : inputStyle}
                   type="password"
                   name="password"
                   value={formData.password}
@@ -170,8 +208,12 @@ export default function Register() {
                         className={`absolute top-0 left-0 h-full rounded ${
                           passwordStrength === 100
                             ? "bg-green-500"
-                            : passwordStrength >= 50
+                            : passwordStrength >= 75
                             ? "bg-yellow-500"
+                            : passwordStrength >= 50
+                            ? "bg-orange-500"
+                            : passwordStrength >= 25
+                            ? "bg-red-500"
                             : "bg-red-500"
                         }`}
                         style={{ width: `${passwordStrength}%` }}
@@ -184,15 +226,7 @@ export default function Register() {
                               ? "text-yellow-900"
                               : "text-red-500"
                           }`}
-                        >
-                          {passwordStrength === 100
-                            ? "Strong"
-                            : passwordStrength >= 50
-                            ? "Medium"
-                            : passwordStrength >= 25
-                            ? "Weak"
-                            : " "}
-                        </div>
+                        ></div>
                       </div>
                     </div>
                   </div>
@@ -202,15 +236,18 @@ export default function Register() {
             <button
               className={`${
                 formValid ? buttonStyleEnabled : buttonStyleDisabled
-              } mt-6`}
+              } mt-10`}
               type="submit"
               disabled={!formValid}
             >
               Register
             </button>
-            <div className="flex justify-center mt-6 gap-2">
-              <p className="text-gray-300">Already have an account?</p>
-              <Link href="/login" className="underline underline-offset-1">
+            <div className="flex justify-center mt-10 gap-2">
+              <p className="text-[#8B93B0]">Already have an account?</p>
+              <Link
+                href="/login"
+                className="underline underline-offset-1 text-white hover:text-sky-700"
+              >
                 Login
               </Link>
             </div>
