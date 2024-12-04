c8771514e4c58b83946881b470e689b690d132da
Thammaruthai <thammaruthai@gmail.com>
2024-12-04 10:36:34 +0700

    feat: responsive added for register page

--- pages/api/register.js
+++ pages/api/register.js
@@ -41,6 +41,7 @@ export default async function handler(req, res) {
     });
     
     
+    console.log(authError);
     
     if (authError) {
       console.error("Error signing up:", authError);
