fd898a83c9cab7a827229e679977f991e4b5eca3
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-12-19 23:37:26 +0700

    feat: add QR code component and payment intent API

---
+++ pages/api/payment/create-payment-qr-code.js
@@ -0,0 +1,27 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { amount, currency } = req.body;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency,
+        payment_method_types: ["promptpay"],
+      });
+      res.status(200).json({
+        clientSecret: paymentIntent.client_secret,
+        paymentIntentId: paymentIntent.id,
+      });
+    } catch (error) {
+      console.error("Error creating PaymentIntent:", error);
+      res.status(400).json({ error: error.message });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
