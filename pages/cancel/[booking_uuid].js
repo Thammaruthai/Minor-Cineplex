86f11f85af2aecc06b1a30a679a01a748d88b974
Sirawit-Boon <ter.sirawit1997@gmail.com>
2025-01-07 21:28:23 +0700

    feat: fetched cancellation summary in cancel page

--- pages/cancel/[booking_uuid].js
+++ pages/cancel/[booking_uuid].js
@@ -5,13 +5,37 @@ import { useRouter } from "next/router";
 
 export default function Register() {
   const [success, setSuccess] = useState(false);
-
-
-
-
-  
+  const [refund, setRefund] = useState([]);
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const { booking_uuid } = router.query;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          `/api/booking-history/cancellation-summary/${booking_uuid}`
+        );
+
+        setRefund(response.data.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (refund.length === 0) {
+      setLoading(false);
+    }
+    if (booking_uuid) {
+      fetchData();
+    }
+  }, [booking_uuid]);
+
+  console.log(`Refund`, refund)
+
   if (1) {
     return (
       <div className="flex flex-col items-center justify-center gap-6 text-white h-screen bg-slate-900 min-h-[640px] min-w-[300px] animate-fade-in font-robotoCondensed ">
@@ -34,12 +58,14 @@ export default function Register() {
             </div>
             <div className="flex justify-center gap-1">
               <span>Total Refund</span>
-              <span className="font-bold ">THB 0.00</span>
+              <span className="font-bold ">{refund.refund_amount}</span>
             </div>
           </div>
           <button
             className="bg-[#4E7BEE] w-full py-3 mt-4 hover:bg-[#1E29A8]"
-            onClick={() => {router.push(`/`);}}
+            onClick={() => {
+              router.push(`/`);
+            }}
           >
             {"Back to home"}
           </button>
